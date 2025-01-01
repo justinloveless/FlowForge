1250d9467cc353512c514f771ae2e216919d52fb
Justin Loveless <justin.loveless@hp.com>
2025-01-01 14:48:13 -0600

    redo dummy change

--- FlowForge/FlowForge.cs
+++ FlowForge/FlowForge.cs
@@ -33,6 +33,7 @@ public class FlowForge(
     public async Task<IEnumerable<string>> GetAssignedActorsAsync(string stateName, WorkflowInstanceId workflowInstanceId) =>
         await assignmentResolver.GetAssignmentsAsync(stateName, workflowInstanceId);
 
+    public bool DummyChange() => true;
 
 
 }

cc9d04720f0ac7b80eb10f0424233d020a134544
Justin Loveless <justin.loveless@hp.com>
2025-01-06 20:53:38 -0600

    create fork and join states

---
+++ FlowForge/Entities/ForkState.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowForge;
+
+public class ForkState : StateDefinition
+{
+    public string Type { get; set; } = "Fork";
+    public List<string> ParallelPaths { get; set; } = [];
+    public string JoinState { get; set; }
+    
+}
---
+++ FlowForge/Entities/JoinState.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowForge;
+
+public class JoinState: StateDefinition
+{
+    public string Type { get; set; } = "Join";
+    public List<string> WaitForEvents { get; set; } = [];
+    public WorkflowEventId ForkEventId { get; init; } = new(Guid.Empty);
+}
